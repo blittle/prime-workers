7c8e74d1bff9dd1d79eb9c5f5c3222674e021485
Bret Little <bret.little@gmail.com>
2013-02-21 20:26:19 -0700

    Separate value generation from rendering so that values can be cached

--- prime.js
+++ prime.js
@@ -3,32 +3,33 @@
 
 	"use strict";
 
-    function drawArc(context, x, y, r, i) {
+    function drawArc(context, x, y, val) {
+        var r = val.val;
         context.beginPath();
         context.arc(x, y, size < startSize ? startSize : size, 0, 2 * Math.PI, false);
         var color = r === 2 ? "red" : "#"+(16777215 - Math.ceil(10000 * Math.log(r)) ).toString(16);
         
         context.fillStyle = color;
         context.fill();
- //       context.fillStyle = 'black';
-  //      context.fillText(i, x, y);
+        //context.fillStyle = 'black';
+        //context.fillText(val.val, x, y);
     }	
 
     function totalDivisors(val) {
-        var count = 0;
+        var count = 1;
 
-        for(var i = val; i > 0; i--) {
+        for(var i = Math.ceil(val/2); i > 0; i--) {
             if (!(val % i)) count++;
         }
 
         return count;
     }
 
-    function drawValues() {
+    function genValues() {
 
-        context.clearRect(0, 0, window.width - 50, window.height -50);
+        iterations++;
 
-        for(var i=1; i < n; i++) {
+        for(var i=(((iterations-1)*n) - 1) + 1; i < (iterations * n); i++) {
             val = totalDivisors(i);
             key = x + 'x' + y;
            
@@ -37,55 +38,100 @@
                     key = (x) + 'x' + (y-1);
                     if(!map[key]) {
                         y--;
-                        map[key] = val;
+                        map[key] = {
+                            val: val,
+                            i: i
+                        };
                         dir = 1; //Switch to up
                     } else {
                         key = (++x) + 'x' + y;
-                        map[key] = val;
+                        if(!map[key]) {
+                            map[key] = {
+                                val: val,
+                                i: i
+                            };
+                        }
                     }
                     break;
                 case 1: // up
                     key = (x - 1) + 'x' + y;
                     if(!map[key]) {
                         x--;
-                        map[key] = val;
+                        map[key] = {
+                            val: val,
+                            i: i
+                        };
                         dir = 2; //Switch to left
                     } else {
                         key = x + 'x' + (--y);
-                        map[key] = val;
+                        if(!map[key]) {
+                            map[key] = {
+                                val: val,
+                                i: i
+                            };
+                        }
                     }
                     break;
                 case 2: // left
                     key = x + 'x' + (y + 1); 
                     if(!map[key]) {
                         y++;
-                        map[key] = val;
+                        map[key] = {
+                            val: val,
+                            i: i
+                        };
                         dir = 3; //Switch to down
                     } else {
                         key = (--x) + 'x' + y;
-                        map[key] = val;
+                        if(!map[key]) {
+                           map[key] = {
+                               val: val, 
+                               i: i
+                           };
+                        }
                     }
                     break;
                 case 3: // down
                     key = (x+1) + 'x' + y;
                     if(!map[key]) {
                         x++;
-                        map[key] = val;
+                        map[key] = {
+                            val: val,
+                            i: i
+                        };
                         dir = 0; //Switch to right
                     } else {
                         key = x + 'x' + (++y);
-                        map[key] = val;
+                        if(!map[key]) {
+                            map[key] = {
+                                val: val,
+                                i: i
+                            };
+                        }
                     }
                     break;
             }
-
-            drawArc(context, (size*2)*x + p, (size*2)*y + p, val, i);
         }	
     }
 
+    function drawValues() {
+        var x, y;
+
+        context.clearRect(0, 0, document.width - 50, document.height -50);
+
+        for(var val in map) {
+            x = val.substring(0, val.indexOf('x'));
+            y = val.substring(val.indexOf('x')+1);
+
+            drawArc(context, (size*2)*x + p, (size*2)*y + p, map[val]);
+        }
+
+    }
+
     var position = {},
-        n = 5000, startSize = 10, size = startSize, 
-        p = 6 * size;
+        n = 5000, startSize = 5, size = startSize, 
+        p = 4 * Math.sqrt(n),
+        scale = 1, iterations = 0;
 
 	var canvas = document.getElementById('canvas');
     var context = canvas.getContext('2d');
@@ -97,12 +143,17 @@
 	var val, key, x = 0, y = 0, dir = 0;
     var map = {};
     
-    drawValues();
-    
-    window.onmousewheel = function(e) {
-        size = Math.abs(e.wheelDelta) / 100 * startSize;
-        drawValues();
-    };
+    genValues();
+    drawValues(); 
 
+    window.onclick = function(e) {
+        genValues();
+        drawValues();    
+    }
+
+    window.onmousewheel = function(e) {
+    //    size = (e.wheelDelta / 1000) + scale;
+    //    drawValues();
+    };
 
 })(this);
